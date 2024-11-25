becaaf42ac90cea49a98747296646c1a5347d24b
Zach Upstone <zu213@bath.ac.uk>
2024-11-25 20:30:44 +0000

    Very poorly formatted spotify albums covers that play songs

--- spotify-game/src/App.js
+++ spotify-game/src/App.js
@@ -12,6 +12,8 @@ function App() {
   const [token, setToken] = useState("")
   const [searchKey, setSearchKey] = useState("")
   const [artists, setArtists] = useState([])
+  const [track, setTrack] = useState(null)
+  const [track2, setTrack2] = useState(null)
 
 
   useEffect(() => {
@@ -47,15 +49,107 @@ function App() {
     })
 
     setArtists(data.artists.items)
+    const id = data.artists.items[0].id;
+    const albums = await axios.get(`https://api.spotify.com/v1/artists/${id}/albums`, {
+      headers: {
+          Authorization: `Bearer ${token}`
+      },
+    })
+    console.log(albums);
+    const id2 = albums.data.items[0].id;
+    const tracks = await axios.get(`https://api.spotify.com/v1/albums/${id2}/tracks`, {
+      headers: {
+          Authorization: `Bearer ${token}`
+      },
+  })
+  setTrack(tracks.data.items[0]);
+  searchSong(e);
 }
 
+const searchSong = async (e) => {
+  e.preventDefault()
+  console.log('aaa')
+  const {data} = await axios.get("https://api.spotify.com/v1/search", {
+      headers: {
+          Authorization: `Bearer ${token}`
+      },
+      params: {
+          q: searchKey,
+          type: "track"
+      }
+  })
+  console.log('test',data.tracks)
+  const track = await axios.get(`https://api.spotify.com/v1/tracks/${data.tracks.items[0].id}`, {
+    headers: {
+        Authorization: `Bearer ${token}`
+    },
+    params: {
+        q: searchKey,
+        type: "track"
+    }
+})
+  console.log('aaaa', track)
+  setTrack2(track.data)
+  
+}
+
+const play = () => {
+  const iframe =document.getElementById('spotify');
+  iframe.contentWindow.postMessage({command: 'toggle'}, '*');
+}
+
+const play2 = () => {
+  const iframe =document.getElementById('spotify2');
+  iframe.contentWindow.postMessage({command: 'toggle'}, '*');
+}
+
+const renderSong = () => {
+  const size =  {width: '100%', height: '100%'}
+  const uri=`spotify:track:${track2 ? track2.id : ' '}`
+  const view='list';
+  const theme='light';
+  
+   return (
+       track2 ?
+       <div key={track2.id} className='Album-cover'>
+               <iframe
+               id="spotify2"
+         title="Spotify"
+         className="player"
+         src={`https://embed.spotify.com/?uri=${uri}&view=${view}&theme=${theme}`}
+         width={size.width}
+         height={size.height}
+         frameBorder="0"
+         allowtransparency="true"
+       />   {track2.album.images.length ? <img onClick={play2} src={track2.album.images[0].url} alt=""/> : <div>No Image</div>}
+           {track2.name}
+       </div> : <div></div>
+   )
+ }
+
 const renderArtists = () => {
-  return artists.map(artist => (
-      <div key={artist.id}>
-          {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
-          {artist.name}
-      </div>
-  ))
+ const size =  {width: '100%', height: '100%'}
+ const uri=`spotify:track:${track ? track.id : ''}`
+ const view='list';
+ const theme='light';
+ 
+  return (
+      artists.length > 0 ?
+      <div key={artists[0].id} className='Album-cover'>
+              <iframe
+              id="spotify"
+        title="Spotify"
+        className="player"
+        src={`https://embed.spotify.com/?uri=${uri}&view=${view}&theme=${theme}`}
+        width={size.width}
+        height={size.height}
+        frameBorder="0"
+        allowtransparency="true"
+      /> 
+          {artists[0].images.length ? <img onClick={play} src={artists[0].images[0].url} alt=""/> : <div>No Image</div>}
+          {artists[0].name}
+      </div> : <div></div>
+  )
 }
 
 
@@ -71,8 +165,10 @@ const renderArtists = () => {
           <form onSubmit={searchArtists}>
           <input type="text" onChange={e => setSearchKey(e.target.value)}/>
           <button type={"submit"}>Search</button>
+          <div id="test"></div>
       </form>
       {renderArtists()}
+      {renderSong()}
 
 
       </div>
