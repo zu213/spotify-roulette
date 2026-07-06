3d358bc3c48e2c91de3bc791b9ea253b6309e1a3
zu213 <hcazenotspu@gmail.com>
2026-07-06 19:07:26 +0100

    Moved to PKCE auth for SpotifyAPI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import {useEffect, useState} from 'react'
 import './styles/App.css'
-import { requestFromSpotify } from './helper/bridge'
+import { requestFromSpotify, getSpotifyAuthUrl, exchangeCodeForToken } from './helper/bridge'
 import { Route, Routes, useNavigate} from 'react-router-dom'
 import Game from './pages/Game'
 import Join from './pages/Join'
@@ -16,32 +16,35 @@ function isJsonString(str) {
 }
 
 function App() {
-  const REDIRECT_URI = "http://localhost:3000"
-  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-  const RESPONSE_TYPE = "token"
+  const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI || "http://127.0.0.1:3000"
 
   const [token, setToken] = useState("")
   const navigate = useNavigate()
 
-  function hashToToken(hash){
-    if (hash && hash.substring(1).split("/").find(elem => elem.startsWith("access_token"))){
-      let token = hash.substring(1).split("/").find(elem => elem.startsWith("access_token")).split("=")[1]
-      window.location.hash = ""
-      let tokenObj = {token, age: Date.now()}
+  async function codeToToken(code){
+    const data = await exchangeCodeForToken(CLIENT_ID, REDIRECT_URI, code).catch(() => null)
+    if (data){
+      window.history.replaceState({}, "", "/")
+      let tokenObj = {token: data.access_token, age: Date.now()}
       window.localStorage.setItem("token", JSON.stringify(tokenObj))
       return tokenObj
     }
   }
 
-  useEffect(() => {
-    const hash = window.location.hash
+  const login = async (e) => {
+    e.preventDefault()
+    window.location = await getSpotifyAuthUrl(CLIENT_ID, REDIRECT_URI)
+  }
+
+  useEffect(() => {(async () => {
+    const code = new URLSearchParams(window.location.search).get("code")
     let tokenObj = window.localStorage.getItem("token")
 
-    if(!hash && !tokenObj) return
+    if(!code && !tokenObj) return
 
-    // If no token and we have a hash in url get the token
+    // If no token and we have a code in the url get the token
     if (!tokenObj){
-      tokenObj = hashToToken(hash)
+      tokenObj = await codeToToken(code)
     } else {
       if(isJsonString(tokenObj)){
         tokenObj = JSON.parse(tokenObj)
@@ -54,8 +57,7 @@ function App() {
     setToken(tokenObj ? tokenObj.token : null)
 
     // poke api to see if token is valid
-    // Didn't work until i chaned the code to log for some reason
-    requestFromSpotify(token, 'me/top/tracks')
+    requestFromSpotify(tokenObj?.token, 'me/top/tracks')
     .then((response)=>{
       console.log('Success! ', tokenObj.token, response)
       navigate("/")
@@ -65,7 +67,7 @@ function App() {
       setToken(null)
     })
 
-  }, [])
+  })()}, [])
 
   const logout = () => {
     navigate("/")
@@ -84,7 +86,7 @@ function App() {
     {!token ?
       <div>
         <h4>You must login to Spotify to use this app</h4>
-        <a className='sign-in-link' href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=user-top-read`}>Login to Spotify</a>
+        <a className='sign-in-link' href='/' onClick={login}>Login to Spotify</a>
       </div>
       :
       <Routes>
--- frontend/src/helper/bridge.js
+++ frontend/src/helper/bridge.js
@@ -16,6 +16,52 @@ export const requestFromSpotify = async (token, endpoint, searchKey=null) => {
   return await axios.get(`https://api.spotify.com/v1/${endpoint}`, requestObject)
 }
 
+function generateRandomString(length) {
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  const values = crypto.getRandomValues(new Uint8Array(length))
+  return values.reduce((acc, x) => acc + possible[x % possible.length], '')
+}
+
+async function generateCodeChallenge(verifier) {
+  const data = new TextEncoder().encode(verifier)
+  const digest = await crypto.subtle.digest('SHA-256', data)
+  return btoa(String.fromCharCode(...new Uint8Array(digest)))
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=+$/, '')
+}
+
+export async function getSpotifyAuthUrl(clientId, redirectUri) {
+  const verifier = generateRandomString(64)
+  window.localStorage.setItem('code_verifier', verifier)
+  const challenge = await generateCodeChallenge(verifier)
+
+  const params = new URLSearchParams({
+    client_id: clientId,
+    response_type: 'code',
+    redirect_uri: redirectUri,
+    scope: 'user-top-read',
+    code_challenge_method: 'S256',
+    code_challenge: challenge
+  })
+
+  return `https://accounts.spotify.com/authorize?${params}`
+}
+
+export async function exchangeCodeForToken(clientId, redirectUri, code) {
+  const body = new URLSearchParams({
+    client_id: clientId,
+    grant_type: 'authorization_code',
+    code: code,
+    redirect_uri: redirectUri,
+    code_verifier: window.localStorage.getItem('code_verifier')
+  })
+
+  const response = await axios.post('https://accounts.spotify.com/api/token', body)
+  window.localStorage.removeItem('code_verifier')
+  return response.data
+}
+
 export const SERVER_URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:5000'
 export const SERVER_WS_URL = SERVER_URL.replace(/^http/, 'ws')
 
